3fe0b56e6492a0d7182bfdf01770f0809ec75489
Nikita Makarov <nikita.makarov@effective-soft.com>
2015-02-16 11:54:38 +0300

    Support New Akuma-User-Bundle

---
+++ src/AppBundle/Entity/Role.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User  : Nikita.Makarov
+ * Date  : 2/16/15
+ * Time  : 5:51 AM
+ * E-Mail: nikita.makarov@effective-soft.com
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Role
+ *
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="role")
+ *
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Role extends \Akuma\Bundle\UserBundle\Entity\Role
+{
+
+} 
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User  : Nikita.Makarov
+ * Date  : 2/16/15
+ * Time  : 5:51 AM
+ * E-Mail: nikita.makarov@effective-soft.com
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * Class Account
+ *
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ *
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User extends \Akuma\Bundle\UserBundle\Entity\User
+{
+
+} 
